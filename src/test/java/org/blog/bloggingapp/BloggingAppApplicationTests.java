7e0b7b023123471d79607190920b11a6dd735bd3
Haider Ali <haideratish2689@gmail.com>
2024-06-04 23:34:48 +0500

    Initial Project Setup

---
+++ src/test/java/org/blog/bloggingapp/BloggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blog.bloggingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
